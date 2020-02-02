a8207ab959b0294795ea3e204965b46b5e3a2a59
Samuel Muloki <samuellebron0@gmail.com>
2020-02-02 20:02:38 +0300

    [Update] Add modifiers, attributes

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,15 +1,47 @@
 var mongoose = require('mongoose'),
 Schema = mongoose.Schema;
 
+var websiteURl = function(url) {
+    if (!url) {
+        return url
+    } else {
+        if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+            url = 'http://' + url;
+        }
+        return url;
+    }
+}
+
 var userSchema = new Schema({
     firstName: String,
     lastName: String,
-    email: String,
-    username: String,
+    email: {
+        type: String
+    },
+    username: {
+        type: String,
+        trim: true
+    },
+    password: String,
     created: {
         type: Date,
         default: Date.now,
+    },
+    website: {
+        type: String,
+        set: websiteURl,
+        get: websiteURl
     }
 });
+
+userSchema.virtual('fullName').get(function () {
+    return this.firstName + '' + this.lastName;
+}).set(function (fullName) {
+    var splitName = fullName.split(' ');
+    this.firstName = splitName[0] || '';
+    this.lastName = splitName[1] || '';
+});
+
+// userSchema.set('toJSON', { getters: true, virtuals: true });
 
 mongoose.model('User', userSchema);
